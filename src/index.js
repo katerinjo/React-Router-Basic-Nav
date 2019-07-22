ac07a47eed38d4b7b7494b22770676d965353aac
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-22 12:28:32 -0700

    import BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { BrowserRouter as Router } from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
