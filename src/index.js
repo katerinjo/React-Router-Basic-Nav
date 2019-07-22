1b15842c11dc9093b5573f9a756ce45a42533f0e
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-22 12:37:18 -0700

    import from correct place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM, { BrowserRouter as Router } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
