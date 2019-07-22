c0ac758724c45494054eb51491d6438086a4773c
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-22 12:50:28 -0700

    display components based on url

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Router />
-    <Router />
-    <Router />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
